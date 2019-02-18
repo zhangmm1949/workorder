37a3449e77af0783a0657207ff477d09e4054338
zhangmm <704369798@qq.com>
2019-02-18 14:03:00 +0800

    a

---
+++ views/test/invoice.php
@@ -0,0 +1,17 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<div>
+    <form action="/test/invoice" method="post">
+        <input type="text" name="invoice_money">
+        <button type='submit' name='button' class="btn btn-flat">提交</button>
+    </form>
+</div>
+</body>
+</html>
