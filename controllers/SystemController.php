21ee25d81c73afcf438bb81cba413cda01b3c062
zhangmm <704369798@qq.com>
2022-11-23 16:13:19 +0800

    system controller 添加控制

--- controllers/SystemController.php
+++ controllers/SystemController.php
@@ -5,6 +5,7 @@ namespace app\controllers;
 use Yii;
 use app\models\System;
 use app\models\SystemSearch;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -20,6 +21,16 @@ class SystemController extends Controller
     public function behaviors()
     {
         return [
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'actions' => [],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
